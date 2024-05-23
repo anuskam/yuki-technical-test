237e5657c6e5fce63b7a5db89c450dfb824f3eb4
anuskam <anuskam92@gmail.com>
2024-05-23 19:38:30 +0200

    search planet component, view and test

--- src/views/SearchPlanetsView.vue
+++ src/views/SearchPlanetsView.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <SearchPlanet/>
+  <div class="container">
+    <h1 class="text-center text-white font-bold">STAR WARS PLANETS</h1>
+    <router-link :to="{name: 'PlanetsView'}" class="flex">
+      <button class="bg-white p-2 rounded"><i class="fa-solid fa-arrow-left fa-lg mr-2"></i>Go back</button>
+    </router-link>
+    <SearchPlanet/>  
   </div>
 </template>
 
