fa5db6fe8ec0bfca9b086e8e68369713dd762841
anuskam <anuskam92@gmail.com>
2024-05-23 17:45:04 +0200

    add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <template>
-  <PlanetsView v-if="isVisible"/>
-  <SearchPlanetsView v-else/>
+   <router-view />
 </template>
 
 <script setup lang="ts">
-import PlanetsView from './views/PlanetsView.vue';
-import SearchPlanetsView from './views/SearchPlanetsView.vue';
-import type { Ref } from 'vue'
-import { ref } from 'vue'
-
-const isVisible: Ref<boolean> = ref(true)
 
 </script>
